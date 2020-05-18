74f824301b88216ad765cf03d9ebf9029cd12df3
Mark McIntyre <mamcintyre@cabarruscounty.us>
2020-05-18 09:56:07 -0400

    Added Javascript to PHP AJAX call

---
+++ Javascript-To-PHP-AJAX/CALL_LASERFICHE_WORKFLOW.php
@@ -0,0 +1,50 @@
+<?php
+
+    $inputParameter = $_POST["inputParameter"];
+
+    if(isset($inputParameter)) {
+
+        $handle = curl_init();
+
+        $url = "http://WORKFLOW_URL_HERE";
+    
+        $data = ["ParameterCollection" => [["Name" => "inputParameter",
+                                            "Value" => $inputParameter]]];
+    
+        $data = json_encode($data);
+    
+        curl_setopt_array($handle,
+    
+        array(
+            CURLOPT_URL => $url,
+            CURLOPT_POST => true,
+    
+            CURLOPT_HTTPHEADER => array(                                                                          
+                'Content-Type: application/json',                                                                                
+                'Content-Length: ' . strlen($data)
+            ),
+    
+            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_HTTPAUTH => CURLAUTH_NTLM,
+            CURLOPT_USERPWD  => "domain\\XXXXXX:XXXXXXXXX",
+            CURLOPT_RETURNTRANSFER => true
+        )
+    
+      );
+    
+        $output = curl_exec($handle);
+        
+        $statusCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    
+        curl_close($handle);
+    
+        
+        if($statusCode == 200) {
+            echo "WORKFLOW STARTED SUCCESSFULLY";
+        } else {
+            echo "There was a problem starting workflow";
+        }
+
+    }
+
+?>
