8682ac7e76503eea6075abc7b0b7185f39dfac63
Mark McIntyre <mamcintyre@cabarruscounty.us>
2020-05-18 20:49:10 -0400

    Added Python Webhook Example

---
+++ PYTHON_WEBHOOK_EXAMPLE/laserfiche-webhook.py
@@ -0,0 +1,24 @@
+import time
+import requests
+from requests_ntlm import HttpNtlmAuth
+import webhook_listener
+
+
+
+def process_post_request(request, *args, **kwargs):
+
+    workflow_url = "http://WORKFLOW_URL_HERE"
+
+    input_parameter = kwargs["inputParameter"]
+    
+    r = requests.post(workflow_url, json={"ParameterCollection":[{"Name":"testParameter", "Value":input_parameter}]}, auth=HttpNtlmAuth('domain\\XXXXXXX','XXXXXXX'))
+
+    return
+
+webhooks = webhook_listener.Listener(handlers={"GET": process_post_request})
+
+webhooks.start()
+
+while True:
+    print("Still alive...")
+    time.sleep(300)
