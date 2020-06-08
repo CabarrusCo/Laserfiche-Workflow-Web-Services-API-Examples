6acc343593ba8bb9bb22933fbd2a4c7236ed51ca
Mark McIntyre <mamcintyre@cabarruscounty.us>
2020-06-08 09:07:41 -0400

    Minor Refactor for scandir function

--- Javascript-To-PHP-POLL-WORKFLOW-Javascript/GRAB_UUID_STATUSES.php
+++ Javascript-To-PHP-POLL-WORKFLOW-Javascript/GRAB_UUID_STATUSES.php
@@ -4,11 +4,11 @@
     $currentIdsInCirculation = json_decode($currentIdsInCirculation);
 
     $statusArrayForReturn = array();
+    $workflowsInCirculationFiles = scandir("WORKFLOW_TEXT_FILES");
 
     foreach($currentIdsInCirculation as $currentID) {
         $uuidForFile = $currentID->uuid;
-        $workflowsInCirculationFiles = scandir("WORKFLOW_TEXT_FILES");
-
+    
         foreach($workflowsInCirculationFiles as $workflowTextFile) {
 
             if (strpos($workflowTextFile, $uuidForFile) !== false) {
@@ -16,7 +16,6 @@
                 $textFileStatus = explode("_", $workflowTextFile);
                 $textFileStatus = $textFileStatus[1];
                 $textFileStatus = str_replace(".txt", "", $textFileStatus);
-
 
                 array_push($statusArrayForReturn, ["uuid" => $uuidForFile, "status" => $textFileStatus]);
 
