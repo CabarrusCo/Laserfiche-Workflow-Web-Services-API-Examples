7b492d027f82591f3bd341b9c30a8001cc61679a
Mark McIntyre <mamcintyre@cabarruscounty.us>
2020-11-30 11:58:15 -0500

    more cleanup

--- Python/PYTHON_WEBHOOK_EXAMPLE/laserfiche-webhook.py
+++ Python/PYTHON_WEBHOOK_EXAMPLE/laserfiche-webhook.py
@@ -3,16 +3,14 @@ import requests
 from requests_ntlm import HttpNtlmAuth
 import webhook_listener
 
-
+#v=EX09nRA-jE4
 
 def process_get_request(request, *args, **kwargs):
 
     workflow_url = "http://WORKFLOW_URL_HERE"
-
     input_parameter = kwargs["inputParameter"]
     
     r = requests.post(workflow_url, json={"ParameterCollection":[{"Name":"testParameter", "Value":input_parameter}]}, auth=HttpNtlmAuth('domain\\XXXXXXX','XXXXXXX'))
-
     return
 
 webhooks = webhook_listener.Listener(handlers={"GET": process_get_request})
